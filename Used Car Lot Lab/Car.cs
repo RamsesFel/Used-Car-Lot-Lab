2a213c6e043328962ccfed8a28c5cfd4bb505d00
Kyra Moore <kyra.moore21@yahoo.com>
2024-05-14 15:27:09 -0400

    Car Class

---
+++ Used Car Lot Lab/Car.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Used_Car_Lot_Lab
+{
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public decimal Price { get; set; }
+
+        //constructor 
+        public Car() 
+        {
+
+            Make = "";
+            Model = "";
+            Year = 0;
+            Price = 0m;
+        }
+        public Car(string _make, string _model, int _year, decimal _price)
+        {
+            Make = _make;
+            Model = _model;
+            Year = _year;
+            Price = _price;
+        }
+        public override string ToString()
+        {
+            return $"{Make} {Model} {Year} {Price}";
+        }
+    }
+}
